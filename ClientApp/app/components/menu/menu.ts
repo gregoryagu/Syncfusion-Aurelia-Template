de95d229673b7f41aa2e4442e8084f34a293a65f
Greg Gum <greg.gum@gmail.com>
2018-09-10 06:57:41 -0400

    Updates

--- ClientApp/app/components/menu/menu.ts
+++ ClientApp/app/components/menu/menu.ts
@@ -1,45 +1,8 @@
 ﻿export class Menu {
-
 	type;
-
-	
-
+	target;
 	constructor() {
-		
-	}
-	create() {
-		console.log('Menu has been created.');
-	}
-	mouseOver(event) {
-		let args = event.detail;
-		if (args.ID) {
-			console.log('Menu item id "' + args.ID + '" has been hovered-in.');
-		} else {
-			console.log('Menu item has been hovered-in.');
-		}
-	}
-	mouseOut(event) {
-		let args = event.detail;
-		if (args.ID) {
-			console.log('Menu item id "' + args.ID + '" has been hovered-out.');
-		} else {
-			console.log('Menu item has been hovered-out.');
-		}
-	}
-	menuClick(event) {
-		let args = event.detail;
-		if (args.ID) {
-			console.log('Menu item id "' + args.ID + '" is clicked.');
-		} else {
-			console.log('Menu item is clicked.');
-		}
-	}
-	keyDown(event) {
-		let args = event.detail;
-		if (args.ID) {
-			console.log('Menu item id "' + args.ID + '" has been changed by keys.');
-		} else {
-			console.log('Menu item has been changed by keys.');
-		}
+		this.type = 'ContextMenu';
+		this.target = '#target';
 	}
 }
